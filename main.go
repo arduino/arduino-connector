e58d8e95449ff11811228d7c6faa7c79366774b6
Alberto Iannaccone <alby.ianna@gmail.com>
2018-01-08 17:36:47 +0100

    add updater

--- main.go
+++ main.go
@@ -41,6 +41,7 @@ import (
 
 const (
 	configFile = "./arduino-connector.cfg"
+	version    = "1.0.0"
 )
 
 // Config holds the configuration needed by the application
@@ -52,6 +53,8 @@ type Config struct {
 	ALLProxy   string
 	AuthURL    string
 	APIURL     string
+	updateUrl  string
+	appName    string
 }
 
 func (c Config) String() string {
@@ -74,6 +77,9 @@ func main() {
 	var doRegister = flag.Bool("register", false, "Registers on the cloud")
 	var listenFile = flag.String("listen", "", "Tail given file and report percentage")
 	var token = flag.String("token", "", "an authentication token")
+	flag.StringVar(&config.updateUrl, "updateUrl", "http://localhost/", "")
+	flag.StringVar(&config.appName, "appName", "arduino-connector", "")
+
 	flag.String(flag.DefaultConfigFlagname, "", "path to config file")
 	flag.StringVar(&config.ID, "id", "", "id of the thing in aws iot")
 	flag.StringVar(&config.URL, "url", "", "url of the thing in aws iot")
@@ -130,6 +136,8 @@ func (p program) run() {
 	// - any spawned sketch process'es also have access to them
 	// Note, all_proxy will not be used by any HTTP/HTTPS connections.
 	p.exportProxyEnvVars()
+
+	updateHandler(p.Config)
 
 	// Start nats-server on localhost:4222
 	opts := server.Options{}
---
+++ updater.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+
+	"github.com/arduino/arduino-connector/updater"
+	"github.com/kardianos/osext"
+)
+
+func updateHandler(config Config) {
+
+	path, err := osext.Executable()
+
+	if err != nil {
+		//c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	var up = &updater.Updater{
+		CurrentVersion: version,
+		APIURL:         config.updateUrl,
+		BinURL:         config.updateUrl,
+		DiffURL:        "",
+		Dir:            "update/",
+		CmdName:        config.appName,
+	}
+
+	err = up.BackgroundRun()
+
+	if err != nil {
+		return
+	}
+
+	//c.JSON(200, gin.H{"success": "Please wait a moment while the agent reboots itself"})
+	go restart(path)
+}
+
+func restart(path string) {
+	log.Println("called restart", path)
+	// relaunch ourself and exit
+	// the relaunch works because we pass a cmdline in
+	// that has serial-port-json-server only initialize 5 seconds later
+	// which gives us time to exit and unbind from serial ports and TCP/IP
+	// sockets like :8989
+	log.Println("Starting new spjs process")
+
+	// figure out current path of executable so we know how to restart
+	// this process using osext
+	exePath, err3 := osext.Executable()
+	if err3 != nil {
+		log.Printf("Error getting exe path using osext lib. err: %v\n", err3)
+	}
+
+	if path == "" {
+		log.Printf("exePath using osext: %v\n", exePath)
+	} else {
+		exePath = path
+	}
+
+	exePath = strings.Trim(exePath, "\n")
+
+	cmd := exec.Command(exePath)
+
+	fmt.Println(cmd)
+
+	err := cmd.Start()
+	if err != nil {
+		log.Printf("Got err restarting spjs: %v\n", err)
+	}
+	log.Fatal("Exited current spjs for restart")
+}
