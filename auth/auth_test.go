fe96df91095d540a60b6296d30e3b6e2177333cd
Federico Guerinoni <guerra@develer.com>
2020-08-17 12:11:15 +0200

    Add test for CheckDeviceAuth
    Signed-off-by: Federico Guerinoni <guerra@develer.com>

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -105,3 +105,66 @@ func TestAuthStartData(t *testing.T) {
 
 	assert.Equal(t, data, d)
 }
+
+func TestAuthCheck(t *testing.T) {
+	type AuthAccess struct {
+		AccessToken string `json:"access_token"`
+		ExpiresIn   int    `json:"expires_in"`
+		TokenType   string `json:"token_type"`
+	}
+
+	DoFunc = func(req *http.Request) (*http.Response, error) {
+		header := req.Header.Values("content-type")
+		if len(header) != 1 {
+			return nil, errors.New("content-type len is wrong")
+		}
+
+		if header[0] != "application/x-www-form-urlencoded" {
+			return nil, errors.New("content-type is wrong")
+		}
+
+		if req.Method != "POST" {
+			return nil, errors.New("Method is wrong")
+		}
+
+		if !strings.Contains(req.URL.Path, "/oauth/token") {
+			return nil, errors.New("url is wrong")
+		}
+
+		body, err := req.GetBody()
+		if err != nil {
+			return nil, err
+		}
+		buf := new(strings.Builder)
+		_, err = io.Copy(buf, body)
+		if err != nil {
+			return nil, err
+		}
+
+		if !strings.Contains(buf.String(), "client_id=") ||
+			!strings.Contains(buf.String(), "grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Adevice_code&device_code=") {
+			return nil, errors.New("Payload is wrong")
+		}
+
+		data, err := json.Marshal(AuthAccess{
+			AccessToken: "asdf",
+			ExpiresIn:   999,
+			TokenType:   "testType",
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		return &http.Response{
+			StatusCode: 200,
+			Body:       ioutil.NopCloser(bytes.NewBufferString(string(data))),
+		}, nil
+	}
+
+	token, err := CheckDeviceAuth("", "0", "")
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Equal(t, "asdf", token)
+}
