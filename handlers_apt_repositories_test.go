61735fe7707d8aadbd5f866d02d2443868921820
Federico Guerinoni <guerra@develer.com>
2020-08-27 14:00:51 +0200

    Add AptAdd test with right data
    Signed-off-by: Federico Guerinoni <guerra@develer.com>

--- handlers_apt_repositories_test.go
+++ handlers_apt_repositories_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"strings"
 	"testing"
 	"time"
 
+	apt "github.com/arduino/go-apt-client"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/stretchr/testify/assert"
 )
@@ -47,4 +49,34 @@ func TestAptAddError(t *testing.T) {
 
 	assert.True(t, strings.HasPrefix(resp, "ERROR"))
 	assert.True(t, strings.Contains(resp, "Unmarshal"))
+}
+
+func TestAptAdd(t *testing.T) {
+	ui := NewMqttTestClientLocal()
+	defer ui.Close()
+
+	s := NewStatus(program{}.Config, nil, nil, "")
+	s.mqttClient = mqtt.NewClient(mqtt.NewClientOptions().AddBroker("tcp://localhost:1883").SetClientID("arduino-connector"))
+	if token := s.mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+	defer s.mqttClient.Disconnect(100)
+
+	subscribeTopic(s.mqttClient, "0", "/apt/repos/add/post", s, s.AptRepositoryAddEvent, true)
+
+	var params struct {
+		Repository *apt.Repository `json:"repository"`
+	}
+
+	params.Repository = &apt.Repository{
+		URI: "www.test.io",
+	}
+
+	data, err := json.Marshal(params)
+	if err != nil {
+		t.Error(err)
+	}
+
+	resp := ui.MqttSendAndReceiveTimeout(t, "/apt/repos/add", string(data), 1*time.Second)
+	assert.Equal(t, "INFO: OK\n", resp)
 }
