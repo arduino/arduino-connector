4fd7f30aca9c8c48c7cccae4912de5ed8c89484e
Federico Guerinoni <guerra@develer.com>
2020-08-27 16:56:49 +0200

    WIP: Add remove tests, but strange behavior
    It seems that repository is already removed but is not right because in
    /etc/apt/sources.list.d/managed.list is still present.
    
    Signed-off-by: Federico Guerinoni <guerra@develer.com>

--- handlers_apt_repositories_test.go
+++ handlers_apt_repositories_test.go
@@ -79,4 +79,59 @@ func TestAptAdd(t *testing.T) {
 
 	resp := ui.MqttSendAndReceiveTimeout(t, "/apt/repos/add", string(data), 1*time.Second)
 	assert.Equal(t, "INFO: OK\n", resp)
+
+	// TODO: check if is it really added on list, I think apt.ParseAPTConfigFolder("/etc/apt") doens't read
+	// all repository
+}
+
+func TestAptRemoveError(t *testing.T) {
+	ui := NewMqttTestClientLocal()
+	defer ui.Close()
+
+	s := NewStatus(program{}.Config, nil, nil, "")
+	s.mqttClient = mqtt.NewClient(mqtt.NewClientOptions().AddBroker("tcp://localhost:1883").SetClientID("arduino-connector"))
+	if token := s.mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+	defer s.mqttClient.Disconnect(100)
+
+	subscribeTopic(s.mqttClient, "0", "/apt/repos/remove/post", s, s.AptRepositoryRemoveEvent, true)
+
+	resp := ui.MqttSendAndReceiveTimeout(t, "/apt/repos/remove", "{test}", 1*time.Second)
+
+	assert.True(t, strings.HasPrefix(resp, "ERROR"))
+	assert.True(t, strings.Contains(resp, "Unmarshal"))
+}
+
+func TestAptRemove(t *testing.T) {
+	ui := NewMqttTestClientLocal()
+	defer ui.Close()
+
+	s := NewStatus(program{}.Config, nil, nil, "")
+	s.mqttClient = mqtt.NewClient(mqtt.NewClientOptions().AddBroker("tcp://localhost:1883").SetClientID("arduino-connector"))
+	if token := s.mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+	defer s.mqttClient.Disconnect(100)
+
+	subscribeTopic(s.mqttClient, "0", "/apt/repos/remove/post", s, s.AptRepositoryRemoveEvent, true)
+
+	var params struct {
+		Repository *apt.Repository `json:"repository"`
+	}
+
+	params.Repository = &apt.Repository{
+		URI: "www.test.io",
+	}
+
+	data, err := json.Marshal(params)
+	if err != nil {
+		t.Error(err)
+	}
+
+	resp := ui.MqttSendAndReceiveTimeout(t, "/apt/repos/remove", string(data), 1*time.Second)
+	assert.Equal(t, "INFO: OK\n", resp)
+
+	// TODO: check if is it really removed on list, I think apt.ParseAPTConfigFolder("/etc/apt") doens't read
+	// all repository
 }
