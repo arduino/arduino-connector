53debdc2c48605bccfc1e0ef21797e8761cb2a9a
Federico Guerinoni <guerra@develer.com>
2020-09-08 14:23:32 +0200

    Implement uninstall sketches
    Signed-off-by: Federico Guerinoni <guerra@develer.com>

--- uninstall.go
+++ uninstall.go
@@ -1,10 +1,31 @@
 package main
 
-import mqtt "github.com/eclipse/paho.mqtt.golang"
+import (
+	"os"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
 
 // Uninstall remove all service installed and create script to
 // removing application from machine
 func (s *Status) Uninstall(client mqtt.Client, msg mqtt.Message) {
 	data := "OK"
+
+	removeSketches(s)
+
 	s.SendInfo(s.topicPertinence+"/status/uninstall", string(data))
+}
+
+func removeSketches(s *Status) error {
+	folder, err := getSketchFolder(s)
+	if err != nil {
+		return err
+	}
+
+	err = os.RemoveAll(folder)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- uninstall_test.go
+++ uninstall_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 	"testing"
 	"time"
 
@@ -9,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUninstallAPI(t *testing.T) {
+func TestUninstallSketches(t *testing.T) {
 	dashboard := newMqttTestClientLocal()
 	defer dashboard.Close()
 
@@ -21,7 +22,28 @@ func TestUninstallAPI(t *testing.T) {
 	defer s.mqttClient.Disconnect(100)
 
 	subscribeTopic(s.mqttClient, "0", "/status/uninstall/post", s, s.Uninstall, false)
+
+	s.config.SketchesPath = "/home"
+	folder, err := getSketchFolder(s)
+	if err != nil {
+		t.Error(err)
+	}
+
+	file, errFile := os.Create(folder + "/fakeSketch")
+	if errFile != nil {
+		t.Error(errFile)
+	}
+
+	file.WriteString("test")
+	file.Close()
+
+	_, err = os.Stat(folder + "/fakeSketch")
+	assert.True(t, err == nil)
+
 	resp := dashboard.MqttSendAndReceiveTimeout(t, "/status/uninstall", "{}", 50*time.Millisecond)
 
+	_, err = os.Stat(folder + "/fakeSketch")
+
 	assert.True(t, resp == "INFO: OK\n")
+	assert.True(t, err != nil)
 }
