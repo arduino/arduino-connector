bd909cdf2cb9bef9ce8ee910943c28a1bdc77e28
Federico Guerinoni <guerra@develer.com>
2020-10-01 18:03:46 +0200

    Remove redundancy tests
    Signed-off-by: Federico Guerinoni <guerra@develer.com>

--- scripts/functional-tests.sh
+++ scripts/functional-tests.sh
@@ -7,3 +7,5 @@ trap 'kill "$(pidof mosquitto)"' EXIT
 mosquitto > /dev/null &
 go test -race -v --tags=functional --run="TestDocker"
 go test -race -v --run="TestUninstall"
+go test -race -v --run=TestApt
+go test -v ./auth
